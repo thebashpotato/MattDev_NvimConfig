17df153ce38a92bdc0577dda06445f82e35c7cb6
johann <johann.suarez92@gmail.com>
2021-02-14 15:03:29 -0800

    Outlined plans for installation shell script

---
+++ check_dependencies.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# First step: Checking what type of linux distro (2 categories supported?)
+# debian OR arch/manjaro bases
+# Command is lsb-release . 
+
+
+# What programs are needed? 
+# For Debian: git, curl, python3-pip, python3-venv, Rust
+# exuberant-ctags, ack-grep
+# Pip packages: pynvim flake8 pylint isort 
+
+
+# For Arch/Manjaro: git, curl, python3, pip, ctags, Rust
+# Pip packages: pynvim flake8 isort  
+
+
+# Where do you get Cargo? Does it come with Rust?
+which python
